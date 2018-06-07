01399d1cd14f21ab4011cdee3ee154f6f9a7c661
Yan Wang <wangyan6783@gmail.com>
2018-06-07 20:45:51 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,8 @@
 def fizzbuzz(num)
-  if num % 3 == 0
+  if num % 3 == 0 && num % 5 != 0
     "Fizz"
-  elsif num % 
+  elsif num % 5 == 0 && num % 3 != 0
+    "Buzz"
+  elsif num % 3 == 0 && num % 5 == 0
+    "FizzBuzz"
 end
